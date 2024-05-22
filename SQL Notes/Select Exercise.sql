6b2b52add68b09c691c9ef8795e78a11291c875f
Srikanth <143884773+guntasrikanth@users.noreply.github.com>
2024-05-22 18:12:40 +0530

    Create Select Exercise.sql

---
+++ SQL Notes/Select Exercise.sql
@@ -0,0 +1,14 @@
+-- Select the information from the “dept_no” column of the “departments” table.
+
+SELECT
+    dept_no
+FROM
+    departments; 
+    
+    
+-- Select all data from the “departments” table. 
+
+SELECT
+    *
+FROM
+    departments; 
